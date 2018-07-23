d65d80f66e824420e73b8421be6232730eb6b55c
Nathan <nathancleon@gmail.com>
2018-07-23 12:29:49 -0400

    added POST method

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -16,6 +16,24 @@ router.get("/", (req, res) => {
   res.json(BlogPosts.get());
 });
 
+router.post("/", (req, res) => {
+  const requiredFields = ["title", "content", "author"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
+  res.status(201).json(item);
+});
+
 router.put("/:id", (req, res) => {
   const requiredFields = ["title", "content", "author"];
   for (let i=0; i<requiredFields.length; i++) {
